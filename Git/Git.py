0d6c5bc15cd95a3e68eb7e0e2332dfc019882656
JiNXxo <Pilveraleonald@gmail.com>
2025-02-06 19:29:43 +0800

    Git

---
+++ Git/Git.py
@@ -0,0 +1,10 @@
+# git status
+# git add .
+# git commit -m "Functions and Modules"
+# git push
+# git restore "FILE.NAME"
+# git restore . (ALL)
+# git log
+# git diff
+# git config --global user.email "
+# git config --global "user.name" "Moses Gitau"
